8953e1ad88ca8242d2250c8f01cd66f23b404d78
RivkiAuerbach <rivki5760@gmail.com>
2024-05-28 21:58:01 +0300

    my commit message

---
+++ .angular/cache/17.3.2/vite/deps/@angular_material_select.js
@@ -0,0 +1,53 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-LOC6L34F.js";
+import "./chunk-H5PXV6KA.js";
+import "./chunk-7A5XRGAA.js";
+import "./chunk-6PA5VECM.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-RMLC5BFB.js";
+import "./chunk-HZDJ64LA.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-G2XLETFR.js";
+import "./chunk-3K5QQA3N.js";
+import "./chunk-FMFASVEP.js";
+import "./chunk-XX7XT7BK.js";
+import "./chunk-WKYGNSYM.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
